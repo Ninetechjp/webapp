ca702c120c0080d322a30a2d376f2993b9c1bfcb
Ninetechjp <ninetechjp@gmail.com>
2020-01-20 02:09:19 +0900

    10.2 認可 終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
       render 'new'
     end
-  end
-
-  def log_out
-    # code here
   end
 
   def destroy
